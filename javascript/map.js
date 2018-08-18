7faaaacacedabf4020afc78de35f1dc97f8ca236
Keren Cerezo <kerencerezo@wifi_guest-10-38-114-60.uncc.edu>
2018-08-18 12:44:28 -0400

    new

--- javascript/map.js
+++ javascript/map.js
@@ -1,6 +1,6 @@
 
   $.ajax({
-    url: "http://beermapping.com/webservice/locimage/636b53943512870ac18caa14a2ba971e/",
+    url:"https://maps.googleapis.com/maps/api/js?key=AIzaSyAIEVe6sKTgq2T9qLw3bS9cUf4bFY1hvOs&callback=initMap",
     method: "GET"
   }).then(function(response) {
     console.log(response);
